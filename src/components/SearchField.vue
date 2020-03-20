c1097b8a9edbad2ac742716bf9edd475fb1e462c
jensen-ali-suliman <alxso@LAPTOP-92UALMD9.(none)>
2020-03-20 09:02:42 +0100

    Create SearchField.vue

---
+++ src/components/SearchField.vue
@@ -0,0 +1,26 @@
+<template>
+  <section>
+    <input type="text" v-model="house" />
+    <button @click="sendUserInput">search</button>
+  </section>
+</template>
+
+<script>
+export default {
+  data: () => {
+    return {
+      house: ""
+    };
+  },
+  methods: {
+    sendUserInput: function() {
+      console.log(this.house);
+      this.$store.dispatch("getCharByHouse", this.house);
+      console.log(this.$store.state.characters.data);
+    }
+  }
+};
+</script>
+
+<style>
+</style>
